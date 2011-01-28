94bea9967de843215919638d9bc68aca3a10d6de
Theofilos Intzoglou <int.teo@gmail.com>
2011-01-28 19:21:14 +0200

    * Break on control-c

--- src/emergemon.py
+++ src/emergemon.py
@@ -5,8 +5,11 @@ __author__="Theofilos Intzoglou"
 __date__ ="$18 Ιαν 2011 5:24:40 μμ$"
 
 import sys
+import signal
 from lib.emmainwin import emMainWidget
 from PyQt4 import QtGui
+
+signal.signal(signal.SIGINT, signal.SIG_DFL) # Break on control-c
 
 if __name__ == "__main__":
     print "EmergeMon Starting... Check your systray!"
